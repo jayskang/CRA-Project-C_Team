8edc7e1c5f576c67b1f8615d22c24fc8daff24ef
hyunjung <gusejddl519@naver.com>
2024-05-03 13:45:11 +0900

    [read] 주소입력범위예외체크(GREEN)

--- SSD/src/main/java/read/ReadModule.java
+++ SSD/src/main/java/read/ReadModule.java
@@ -1,4 +1,13 @@
 package read;
 
+import static java.lang.Integer.parseInt;
+
 public class ReadModule {
+
+    public void read(String str) {
+        String command[] = str.split(" ");
+        if(parseInt(command[2])>100){
+            throw new RuntimeException("주소 입력이 잘못되었습니다.");
+        }
+    }
 }
