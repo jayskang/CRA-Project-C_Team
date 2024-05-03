015faffccfcc130d8fa29b7a5d6c027fe2740a63
jays.kang <jays.kang@samsung.com>
2024-05-03 15:47:19 +0900

    ssd 비정상 케이스에서 Exception 발생하지 않도록 수정 정상 케이스에 대항 Mocking 추가

--- SSD/src/main/java/command/Commander.java
+++ SSD/src/main/java/command/Commander.java
@@ -1,36 +1,46 @@
 package command;
 
 import jdk.jfr.Unsigned;
+import read.ReadCore;
+import write.WriteCore;
 
 public class Commander {
+    private ReadCore readCore;
+    private WriteCore writeCore;
+
     private String command;
-    private String lba;
+    private int lba;
     private String inputData;
 
-    public Commander(String[] args) {
+    public Commander(String[] args, ReadCore readCore, WriteCore writeCore) {
         if(args.length == 0 || args.length > 3) {
-            throw new IllegalArgumentException();
+            return;
         }
 
         command = args[0];
-        lba = args[1];
+        lba = Integer.parseInt(args[1]);
         if(command.equals("W")) {
             try {
                 inputData = args[2];
             } catch (ArrayIndexOutOfBoundsException e) {
-                throw new IllegalArgumentException();
+                return;
             }
         }
+
+        this.readCore = readCore;
+        this.writeCore = writeCore;
     }
 
     public void runCommand() {
         switch (command) {
             case "R":
+                readCore.read(lba);
                 break;
             case "W":
+                writeCore.write(lba, inputData);
                 break;
             default:
-                throw new IllegalArgumentException();
+                return;
         }
     }
 }
--- SSD/src/test/java/command/CommanderTest.java
+++ SSD/src/test/java/command/CommanderTest.java
@@ -1,43 +1,79 @@
 package command;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import read.ReadCore;
+import write.WriteCore;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CommanderTest {
+    @Mock
+    ReadCore readCore;
+
+    @Mock
+    WriteCore writeCore;
+    private Commander commander;
+
+    private Commander getCommander(String[] args) {
+        return new Commander(args, readCore, writeCore);
+    }
+
+    private void verifySsdNotWork() {
+        verify(readCore, never()).read(anyInt());
+        verify(writeCore, never()).write(anyInt(), anyString());
+    }
+
     @Test
-    void noArgumentTest() {
+    void 입력값이_없을때() {
         String[] args = {};
+        commander = getCommander(args);
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            Commander commander = new Commander(args);
-        });
+        verifySsdNotWork();
     }
 
     @Test
-    void over3ArgumentTest() {
+    void 입력값이_3개가_넘을때() {
         String[] args = {"W", "0", "0x00000001", "0x00000001"};
+        commander = getCommander(args);
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            Commander commander = new Commander(args);
-        });
+        verifySsdNotWork();
     }
 
     @Test
-    void invalidCommandNameTest() {
-        String[] args = {"A", "0x00000000"};
-        Commander commander = new Commander(args);
+    void 정해진_명령어가_아닐떄() {
+        String[] args = {"A", "0", "0x00000000"};
+        commander = getCommander(args);
 
-        assertThrows(IllegalArgumentException.class, commander::runCommand);
-
+        verifySsdNotWork();
     }
 
     @Test
-    void writeCommandButNoInputData() {
-        String [] args = {"W", "0"};
+    void 명령어가_W지만_데이터가_없을때() {
+        String[] args = {"W", "0"};
+        commander = getCommander(args);
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            Commander commander = new Commander(args);
-        });
+        verifySsdNotWork();
+    }
+
+    @Test
+    void 정상적인_W명령어_입력됐을때() {
+        String[] args = {"W", "0", "0x00000001"};
+        commander = getCommander(args);
+        commander.runCommand();
+
+        verify(writeCore, times(1)).write(0, "0x00000001");
+    }
+
+    @Test
+    void 정상적인_R명령어_입력됐을때() {
+        String[] args = {"R", "0"};
+        commander = getCommander(args);
+        commander.runCommand();
+
+        verify(readCore, times(1)).read(0);
     }
 }
