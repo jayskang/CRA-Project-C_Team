b92be02c684fbbdbd6833acb16c16504845eb092
hyunjung <gusejddl519@naver.com>
2024-05-03 17:10:49 +0900

    [READ] 빈파일 호출 했을때 함수 구현(GREEN)

--- SSD/src/main/java/read/SsdFileReader.java
+++ SSD/src/main/java/read/SsdFileReader.java
@@ -2,9 +2,17 @@ package read;
 
 import cores.AddressConstraint;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class SsdFileReader {
-    public String[] readFile() {
+    public String[] readFile() throws FileNotFoundException {
         String[] result = new String[AddressConstraint.MAX_BOUNDARY];
+
+        Scanner scanner = new Scanner(new File("nand.txt"));
 
         return result;
     }
