b8dc7cc96030404a03d2c238254b570494c975a8
taylous <taylous99@gmail.com>
2024-05-03 14:51:16 +0900

    [Refactoring] mock 적용

--- SSD/src/test/java/write/WriteModuleTest.java
+++ SSD/src/test/java/write/WriteModuleTest.java
@@ -1,42 +1,33 @@
 package write;
 
 import cores.ExceptionMessage;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class WriteModuleTest {
 
     @Mock
-    private WriteModule writeModule;
-
-    @BeforeEach
-    void setUp() {
-        this.writeModule = new WriteModule();
-    }
+    private WriteModule WRITE_MODULE;
 
     @Test
-    void 기본_생성자_함수() {
-        assertNotNull(this.writeModule);
-    }
+    void 주소값이_0_미만일때() {
+        WriteModule writeModule = this.WRITE_MODULE;
 
-    @Test
-    void 주소값이_0_이하일때() {
         int address = -1;
         String value = "0x00000001";
 
         try {
-            this.writeModule.write(address, value);
+            writeModule.write(address, value);
         } catch (IllegalArgumentException illegalArgumentException) {
             assertThat(illegalArgumentException.getMessage()).isEqualTo(ExceptionMessage.ILLEGAL_ADDRESS_VALUE_EXCEPTION_MSG);
         }
+        verify(writeModule, times(1)).write(address, value);
     }
 }
