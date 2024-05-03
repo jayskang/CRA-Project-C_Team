824ea241000b29edbe0d0a80e9d98056c0a55231
hyunjung <gusejddl519@naver.com>
2024-05-03 16:02:02 +0900

    [READ] 빈파일 호출 했을때 UnitTest 추가(RED)

--- SSD/src/main/java/read/SsdFileReader.java
+++ SSD/src/main/java/read/SsdFileReader.java
@@ -3,9 +3,5 @@ package read;
 import cores.AddressConstraint;
 
 public class SsdFileReader {
-    public String[] readFile() {
-        String[] result = new String[AddressConstraint.MAX_BOUNDARY];
 
-        return result;
-    }
 }
