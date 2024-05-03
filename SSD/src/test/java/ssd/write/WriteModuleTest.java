e58f1ccb394c7ae3da369ea334ec6e85cac1264f
taylous <taylous99@gmail.com>
2024-05-03 17:12:54 +0900

    [Refactoring] 테스트 함수 중복 코드 제거
    테스트 함수에서 중복되는 코드를 멤버필드(매직넘버)로 참조할 수 있도록 수정했습니다.

--- SSD/src/test/java/ssd/write/WriteModuleTest.java
+++ SSD/src/test/java/ssd/write/WriteModuleTest.java
@@ -1,5 +1,6 @@
 package ssd.write;
 
+import cores.AddressConstraint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +17,12 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class WriteModuleTest {
 
+    private final String COLLECT_VALUE = "0x00000001";
+    private final String WRONG_FORMAT_VALUE = "00000001";
+    private final String NOT_ALLOWED_VALUE = "!@#$%";
+    private final int COLLECT_ADDRESS = 0;
+    private final int NOT_ALLOWED_ADDRESS = -1;
+
     private WriteModule writeModule;
 
     @Mock
@@ -31,63 +38,43 @@ class WriteModuleTest {
 
     @Test
     void 주소값이_0_미만일때() {
-        int address = -1;
-        String value = "0x00000001";
-
-        this.writeModule.write(address, value);
+        this.writeModule.write(NOT_ALLOWED_ADDRESS, COLLECT_VALUE);
 
         assertThat(this.writeModule.getErrorLog().getClass()).isEqualTo(IllegalArgumentException.class);
     }
 
     @Test
     void 주소값이_100_이상일때() {
-
-        int address = 100;
-        String value = "0x00000001";
-
-        writeModule.write(address, value);
+        writeModule.write(AddressConstraint.MAX_BOUNDARY, COLLECT_VALUE);
 
         assertThat(this.writeModule.getErrorLog().getClass()).isEqualTo(IllegalArgumentException.class);
     }
 
     @Test
     void 저장할_입력값이_포맷을_벗어났을때() {
-
-        int address = 0;
-        String value = "00000001";
-
-        this.writeModule.write(address, value);
+        this.writeModule.write(COLLECT_ADDRESS, WRONG_FORMAT_VALUE);
 
         assertThat(this.writeModule.getErrorLog().getClass()).isEqualTo(NumberFormatException.class);
     }
 
     @Test
     void 입력값에_이상한_값이_있을때() {
-
-        int address = 0;
-        String value = "!@#$%";
-
-        this.writeModule.write(address, value);
+        this.writeModule.write(COLLECT_ADDRESS, NOT_ALLOWED_VALUE);
 
         assertThat(this.writeModule.getErrorLog().getClass()).isEqualTo(NumberFormatException.class);
     }
 
     @Test
     void 값이_제대로_저장되었는지_테스트() {
-
-        int address = 0;
-        String value = "0x1234ABCD";
-
         String[] readFileContents = new String[]{"0x1234ABCD"};
-        String expected = "0x1234ABCD";
 
         when(this.ssdFileReader.readFile()).thenReturn(readFileContents);
 
-        this.writeModule.write(address, value);
-        this.READ_MODULE.read(address);
+        this.writeModule.write(COLLECT_ADDRESS, "0x1234ABCD");
+        this.READ_MODULE.read(COLLECT_ADDRESS);
 
         String[] readFileResult = this.ssdFileReader.readFile();
 
-        assertThat(readFileResult[address]).isEqualTo(expected);
+        assertThat(readFileResult[COLLECT_ADDRESS]).isEqualTo("0x1234ABCD");
     }
 }
