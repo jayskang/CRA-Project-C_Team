4035ad3af12d2bbe66c57c28006490bb6753fa05
hyunjung <gusejddl519@naver.com>
2024-05-03 13:27:43 +0900

    [read] 주소입력범위예외체크 test 함수 생성(RED)

--- SSD/src/test/java/MainTest.java
+++ SSD/src/test/java/MainTest.java
@@ -1,3 +1,22 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import read.ReadModule;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class WriteModuleTest {
+
+}
+
+class ReadModuleTest {
+    @Test
+    void 주소입력범위예외체크() {
+        ReadModule readModule = new ReadModule();
+        assertThrows(RuntimeException.class, () -> {
+            readModule.read("ssd R 192");
+        });
+    }
+
 
 }
