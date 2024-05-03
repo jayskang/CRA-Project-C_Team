7414ae86955e114bc74662d330d40d90275215a7
hyunjung <gusejddl519@naver.com>
2024-05-03 17:10:26 +0900

    [READ] 빈파일 호출 했을때 UnitTest 추가(RED)

--- SSD/src/test/java/ReadModuleTest.java
+++ SSD/src/test/java/ReadModuleTest.java
@@ -1,3 +1,6 @@
+import cores.AddressConstraint;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
@@ -11,6 +14,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +24,21 @@ class ReadModuleTest {
     @Spy
     private ReadModule readModule;
 
+    private  File file;
+    private FileWriter fw;
+    private  SsdFileReader ssdFileReader;
+    private String result[];
+
+    @BeforeEach
+    void setUp() throws IOException {
+        file = new File("nand.txt");
+        file.createNewFile();
+        fw = new FileWriter(file);
+
+        ssdFileReader = new SsdFileReader();
+
+        result = new String[100];
+    }
 
     @Test
     void 주소입력범위예외체크() {
@@ -27,7 +47,24 @@ class ReadModuleTest {
         verify(readModule,times(1)).isValidAddress(192);
     }
 
+    @Test
+    void 빈파일_호출했을때() throws IOException {
+        createWriteSampleFile("");
+        String expected[] = new String[100];
+        for(int index=0;index< AddressConstraint.MAX_BOUNDARY;index++){
+            expected[index]=null;
+        }
 
+        result = ssdFileReader.readFile();
+
+        assertArrayEquals(expected,result);
+    }
+
+    private void createWriteSampleFile(String writeSample) throws IOException {
+        BufferedWriter writer = new BufferedWriter(fw);
+        writer.write(writeSample);
+        writer.close();
+    }
 
 
 }
