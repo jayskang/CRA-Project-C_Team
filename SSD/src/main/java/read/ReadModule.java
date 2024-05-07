2694c437afa0085cc2297ebabf74a149edba2814
hyunjung <gusejddl519@naver.com>
2024-05-07 10:30:18 +0900

    [READ] 결과파일생성 구현(GREEN)

--- SSD/src/main/java/read/ReadModule.java
+++ SSD/src/main/java/read/ReadModule.java
@@ -2,10 +2,17 @@ package read;
 
 import cores.SSDConstraint;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static cores.SSDConstraint.*;
 import static java.lang.Integer.parseInt;
 
 
 public class ReadModule implements ReadCore {
+    private File file;
+    private FileWriter fileWriter;
 
     public void read(int lba) {
 
@@ -13,10 +20,17 @@ public class ReadModule implements ReadCore {
             return;
         }
 
+        file = new File(RESULT_FILENAME);
+        try {
+            fileWriter = new FileWriter(file);
+        } catch (IOException e) {
+            return;
+        }
+
 
     }
 
     public boolean isValidAddress(int lba) {
-        return lba >= SSDConstraint.MAX_BOUNDARY || lba < SSDConstraint.MIN_BOUNDARY;
+        return lba >= MAX_BOUNDARY || lba < MIN_BOUNDARY;
     }
 }
