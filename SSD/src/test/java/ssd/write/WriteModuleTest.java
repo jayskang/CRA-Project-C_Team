40a4687f5e23bc5869d21431b7095c72ada9bf69
taylous <taylous99@gmail.com>
2024-05-07 15:50:21 +0900

    [Refactoring] WriteModule 테스트 함수에서 중복되는 코드 제거

--- SSD/src/test/java/ssd/write/WriteModuleTest.java
+++ SSD/src/test/java/ssd/write/WriteModuleTest.java
@@ -1,6 +1,5 @@
 package ssd.write;
 
-import cores.SSDConstraint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +9,10 @@ import read.SsdFileReader;
 import write.WriteModule;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import static cores.SSDConstraint.DEFAULT_VALUE;
+import static cores.SSDConstraint.MAX_BOUNDARY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -25,62 +27,67 @@ class WriteModuleTest {
 
     private WriteModule writeModule;
 
+    private String[] nand;
+
     @Mock
     private SsdFileReader ssdFileReader;
 
     @BeforeEach
     void setUp() {
         this.writeModule = new WriteModule();
+        this.nand = createDefaultNand();
     }
 
     @Test
     void 주소값이_0_미만일때() throws IOException {
-        String[] readFileContents = new String[]{};
         this.writeModule.write(NOT_ALLOWED_ADDRESS, COLLECT_VALUE);
 
-        when(this.ssdFileReader.readFile()).thenReturn(readFileContents);
+        when(this.ssdFileReader.readFile()).thenReturn(this.nand);
 
-        assertThat(this.ssdFileReader.readFile()).isEqualTo(readFileContents);
+        assertThat(this.ssdFileReader.readFile()).isEqualTo(this.nand);
     }
 
     @Test
     void 주소값이_100_이상일때() throws IOException {
-        String[] readFileContents = new String[]{};
-        this.writeModule.write(SSDConstraint.MAX_BOUNDARY, COLLECT_VALUE);
+        this.writeModule.write(MAX_BOUNDARY, COLLECT_VALUE);
 
-        when(this.ssdFileReader.readFile()).thenReturn(readFileContents);
+        when(this.ssdFileReader.readFile()).thenReturn(this.nand);
 
-        assertThat(this.ssdFileReader.readFile()).isEqualTo(readFileContents);
+        assertThat(this.ssdFileReader.readFile()).isEqualTo(this.nand);
     }
 
     @Test
     void 저장할_입력값이_포맷을_벗어났을때() throws IOException {
-        String[] readFileContents = new String[]{};
         this.writeModule.write(COLLECT_ADDRESS, WRONG_FORMAT_VALUE);
 
-        when(this.ssdFileReader.readFile()).thenReturn(readFileContents);
+        when(this.ssdFileReader.readFile()).thenReturn(this.nand);
 
-        assertThat(this.ssdFileReader.readFile()).isEqualTo(readFileContents);
+        assertThat(this.ssdFileReader.readFile()).isEqualTo(this.nand);
     }
 
     @Test
     void 입력값에_이상한_값이_있을때() throws IOException {
-        String[] readFileContents = new String[]{};
         this.writeModule.write(COLLECT_ADDRESS, NOT_ALLOWED_VALUE);
 
-        when(this.ssdFileReader.readFile()).thenReturn(readFileContents);
+        when(this.ssdFileReader.readFile()).thenReturn(this.nand);
 
-        assertThat(this.ssdFileReader.readFile()).isEqualTo(readFileContents);}
+        assertThat(this.ssdFileReader.readFile()).isEqualTo(this.nand);}
 
     @Test
     void 값이_제대로_저장되었는지_테스트() throws IOException {
-        String[] readFileContents = new String[]{"0x1234ABCD"};
+        this.nand[COLLECT_ADDRESS] = "0x1234ABCD";
 
-        when(this.ssdFileReader.readFile()).thenReturn(readFileContents);
+        when(this.ssdFileReader.readFile()).thenReturn(this.nand);
 
         this.writeModule.write(COLLECT_ADDRESS, "0x1234ABCD");
         String[] readFileResult = this.ssdFileReader.readFile();
 
         assertThat(readFileResult[COLLECT_ADDRESS]).isEqualTo("0x1234ABCD");
+    }
+
+    private String[] createDefaultNand() {
+        String[] nand = new String[MAX_BOUNDARY];
+        Arrays.fill(nand, DEFAULT_VALUE);
+        return nand;
     }
 }
