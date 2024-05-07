e3fc5652d57a9a23015325d2f4ea4c98d868cc2c
taylous <taylous99@gmail.com>
2024-05-07 13:51:28 +0900

    [GREEN] WriteModule 기본생성함수 생성 테스트

--- SSD/src/test/java/ssd/ssdFullTest.java
+++ SSD/src/test/java/ssd/ssdFullTest.java
@@ -1,8 +1,18 @@
 package ssd;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import write.WriteModule;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ssdFullTest {
 
+    private WriteModule writeModule;
 
+    @Test
+    void 기본_생성함수() {
+        this.writeModule = new WriteModule();
+        assertNotNull(this.writeModule);
+    }
 }
