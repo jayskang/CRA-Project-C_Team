5ab2cefb1a5022452805f7620cc8b866113ef6be
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-05-07 09:19:34 +0900

    lba와 data의 유효성을 검사하는 로직 일부를 메서드 추출로 리팩토링

--- SsdTestShell/src/main/java/SsdTestShell.java
+++ SsdTestShell/src/main/java/SsdTestShell.java
@@ -19,18 +19,6 @@ public class SsdTestShell implements ISsdCommand{
         }
     }
 
-    private static void checkIsDataValid(String data) throws IllegalArgumentException {
-        boolean invalidDataFormat = !data.startsWith("0x") || data.length() != 10;
-        if (invalidDataFormat) {
-            throw new IllegalArgumentException("INVALID Argument. 2번째 인자가 유효하지 않습니다.");
-        }
-        try {
-            Integer.parseInt(data.substring(2), 16);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("INVALID Argument. 2번째 인자가 유효하지 않습니다.");
-        }
-    }
-
     @Override
     public void read(String lba) {
         try{
@@ -42,16 +30,33 @@ public class SsdTestShell implements ISsdCommand{
         }
     }
 
-    private static void checkIsLbaValid(String lba) throws IllegalArgumentException{
-        int param;
+    private static void checkIsDataValid(String data) throws IllegalArgumentException {
+        if (isInvalidDataFormat(data)) {
+            throw new IllegalArgumentException("INVALID Argument. 2번째 인자가 유효하지 않습니다.");
+        }
         try {
-            param = Integer.parseInt(lba);
+            Integer.parseInt(data.substring(2), 16);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("INVALID Argument. 2번째 인자가 유효하지 않습니다.");
+        }
+    }
+
+    private static boolean isInvalidDataFormat(String data) {
+        return !data.startsWith("0x") || data.length() != 10;
+    }
+
+    private static void checkIsLbaValid(String lba) throws IllegalArgumentException{
+        try {
+            int lbaNum = Integer.parseInt(lba);
+            if(isLbaOutOfRange(lbaNum))
+                throw new IllegalArgumentException("Invalid Argument. 1 번째 인자가 유효하지 않습니다.");
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Invalid Argument. 1 번째 인자가 유효하지 않습니다.");
         }
-        boolean isLbaOutOfRange = param > MAX_LBA || param < MIN_LBA;
-        if(isLbaOutOfRange)
-            throw new IllegalArgumentException("Invalid Argument. 1 번째 인자가 유효하지 않습니다.");
+    }
+
+    private static boolean isLbaOutOfRange(int param) {
+        return param > MAX_LBA || param < MIN_LBA;
     }
 
     public void printError(Exception e) {
