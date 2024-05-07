5b02ffb252cd46c9fe1b1856a8b11ecdfdc57e17
ParkJongNam <gggl.park@gmail.com>
2024-05-07 10:53:50 +0900

    read함수 호출 시 result.txt 파일로부터 lba 및 data 값을 읽어 출력하는 기능 추가를 위한 테스트 코드 및 기능 추가했습니다. Error 발생 시 프린트하지 않고 Throw Exception함. Read한 결과는 자체 출력하지 않고 String을 반환

--- SsdTestShell/src/main/java/SSD.java
+++ SsdTestShell/src/main/java/SSD.java
@@ -1,4 +1,30 @@
+import java.io.IOException;
+
 public class SSD {
-    void write(String lbs, String data) {}
-    void read(Integer lba) {}
+    public static final String ERROR_MSG_RESULT_FILE_NOT_FOUNDED = "Result File Not Founded";
+    SSDResultFileReader resultFileReader;
+
+    void setResultFileReader(SSDResultFileReader reader){
+        this.resultFileReader = reader;
+    }
+
+    public void write(String lbs, String data) {}
+
+    public String read(String lba) {
+        execSsdReadCommand(lba);
+        return readResultFile();
+    }
+
+    public void execSsdReadCommand(String lba) {
+        // 1. ssd java 프로그램을 실행시켜야 함.
+        //    ※ 명령문 : ssd R [LBA]
+    }
+
+    public String readResultFile() {
+        try {
+            return resultFileReader.readFile();
+        } catch (IOException e) {
+            return ERROR_MSG_RESULT_FILE_NOT_FOUNDED;
+        }
+    }
 }
