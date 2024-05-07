5f9045d5bc55edaa454454209ff9590cce52d2f0
taylous <taylous99@gmail.com>
2024-05-07 14:35:08 +0900

    [Refactoring] 객체 생성 코드 before each로 이동

--- SSD/src/test/java/ssd/ssdFullTest.java
+++ SSD/src/test/java/ssd/ssdFullTest.java
@@ -1,6 +1,6 @@
 package ssd;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import read.ReadModule;
 import write.WriteModule;
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 import static cores.SSDConstraint.FILE_ABSOLUTE_LOCATION;
 import static cores.SSDConstraint.RESULT_FILENAME;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ssdFullTest {
@@ -19,20 +19,20 @@ public class ssdFullTest {
     private WriteModule writeModule;
     private ReadModule readModule;
 
-    @Test
-    void 기본_생성함수() {
+    @BeforeEach
+    void setUp() {
         this.writeModule = new WriteModule();
         this.readModule = new ReadModule();
+    }
 
+    @Test
+    void 기본_생성함수() {
         assertNotNull(this.writeModule);
         assertNotNull(this.readModule);
     }
 
     @Test
     void 정상적인_데이터_쓰기() {
-        this.writeModule = new WriteModule();
-        this.readModule = new ReadModule();
-
         this.writeModule.write(0, "0x12341234");
         this.readModule.read(0);
 
