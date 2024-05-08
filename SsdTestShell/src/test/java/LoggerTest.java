b3645bcb3902abd1ededb24fa7957cc77b67e3f4
hyunjung <gusejddl519@naver.com>
2024-05-08 14:52:11 +0900

    Logger Print UnitTest 추가

--- SsdTestShell/src/test/java/LoggerTest.java
+++ SsdTestShell/src/test/java/LoggerTest.java
@@ -1,12 +1,31 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoggerTest {
 
     @Test
     void loggingPrintTest() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+        String methodName = "LoggerTest.loggingPrintTest()";
+
+
+        String expected = LocalDateTime.now().format(DateTimeFormatter.ofPattern("'['yy/MM/dd HH:mm']'"))
+                + " " + methodName + "\t" + "test\n";
+
         Logger log = Logger.makeLog();
         log.logging("test");
+        String actual = outputStream.toString();
+
+        assertEquals(expected.trim(), actual.trim());
+
     }
 }
