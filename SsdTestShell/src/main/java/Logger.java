5969ed52941a149bd3b27fc05959474ce8f633c7
hyunjung <gusejddl519@naver.com>
2024-05-09 09:26:53 +0900

    [Logger] Refactoring 진행 , unit test 추가

--- SsdTestShell/src/main/java/Logger.java
+++ SsdTestShell/src/main/java/Logger.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import static constants.Logging.*;
 
 public class Logger {
     private static BufferedWriter bw;
@@ -16,7 +17,7 @@ public class Logger {
     private static File file;
 
     private Logger() throws IOException {
-        file = new File("latest.log");
+        file = new File(LATEST_LOGFILE_NAME);
         bw = new BufferedWriter(new FileWriter(file));
     }
 
@@ -25,55 +26,82 @@ public class Logger {
         return logger;
     }
 
-    public void logging(String str) throws IOException {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyyMMdd_HH'h'_mm'm'_ss");
-        String newFileName = "until_" + now.format(formatter1) + ".log";
+    public void print(String errMessage) throws IOException {
+        StackTraceElement calledThread = Thread.currentThread().getStackTrace()[2];
 
-        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("'['yy/MM/dd HH:mm']'");
-        String formatedNow2 = now.format(formatter2);
+        printAndWriteLog(getLogMessage(errMessage, calledThread));
 
-
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        String methodName = "";
-        if (stackTraceElements.length > 2) {
-            methodName = stackTraceElements[2].getClassName() +"."+stackTraceElements[2].getMethodName();
-        }
-
-        String log = formatedNow2 + " " + methodName + "\t" + str;
-        bw.write(log + "\n");
-        System.out.println(log);
-        bw.flush();
-
-        long kilobyte= file.length() / 1024;
-
-        if (kilobyte >= 10) {
-            bw.close();
-            File renameFile = new File(newFileName);
-            file.renameTo(renameFile);
-
-            bw = new BufferedWriter(new FileWriter("latest.log"));
-
-            File directory = new File("./");
-            File[] files = directory.listFiles((dir, name) -> name.startsWith("until_") && name.endsWith(".log"));
-            List<File> fileList = new ArrayList<>();
-            if (files != null) {
-                for (File fileObject : files) {
-                    if (fileObject.isFile()) {
-                        fileList.add(fileObject);
-                    }
-                }
-                Collections.sort(fileList, Comparator.comparing(File::getName));
-            }
-            if (fileList.size() >= 2) {
-                String fileOldName = fileList.get(0).getName();
-                String fileNewName = fileOldName.replace("log", "zip");
-                File renameFile2 = new File(fileNewName);
-                File oldFile1 = new File(fileOldName);
-                oldFile1.renameTo(renameFile2);
-            }
+        if (isOver10KB()) {
+            seperateFileForManagingFileSize();
+            checkFileNumberAndZipOldestFile();
         }
     }
+
+    private static String getLogMessage(String errMessage, StackTraceElement calledThread) {
+        return setDateForLogging() + " " + calledThread.getClassName() + "." +
+                calledThread.getMethodName() + "()" + "\t" + errMessage;
+    }
+
+    private static String setDateForLogging() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("'" + LOGGING_DATE_FORMAT));
+    }
+
+    private static void printAndWriteLog(String logMessage) throws IOException {
+        System.out.println(logMessage);
+        bw.write(logMessage + "\n");
+        bw.flush();
+    }
+
+    private static boolean isOver10KB() {
+        return file.length() / BYTES >= MAX_FILE_SIZE;
+    }
+
+    private static void seperateFileForManagingFileSize() throws IOException {
+        bw.close();
+        File renameFile = new File(setNewLogfileName());
+        file.renameTo(renameFile);
+        bw = new BufferedWriter(new FileWriter(LATEST_LOGFILE_NAME, true));
+    }
+
+    private static String setNewLogfileName() {
+        return SEPERATED_LOGFILE_PREFIX
+                + LocalDateTime.now().format(DateTimeFormatter.ofPattern(FILE_NAME_DATE_FORMATTER))
+                + FILE_TYPE_LOG;
+    }
+
+    private static void checkFileNumberAndZipOldestFile() {
+        List<File> fileList = getExistsFileList();
+        if (isMoreThan2Files(fileList)) {
+            zipOldestFile(fileList);
+        }
+    }
+
+    private static List<File> getExistsFileList() {
+        File directory = new File(FILE_PATH);
+        File[] files = directory.listFiles(
+                (dir, name) -> name.startsWith(SEPERATED_LOGFILE_PREFIX) && name.endsWith(FILE_TYPE_LOG));
+        List<File> fileList = new ArrayList<>();
+        if (files != null) {
+            for (File fileObject : files) {
+                if (fileObject.isFile()) {
+                    fileList.add(fileObject);
+                }
+            }
+            Collections.sort(fileList, Comparator.comparing(File::getName));
+        }
+        return fileList;
+    }
+
+    private static boolean isMoreThan2Files(List<File> fileList) {
+        return fileList.size() >= MAX_LOGFILE_NUMBER;
+    }
+
+    private static void zipOldestFile(List<File> fileList) {
+        String oldestFileName = fileList.get(0).getName();
+        File oldestFile = new File(oldestFileName);
+        oldestFile.renameTo(new File(oldestFileName.replace(FILE_TYPE_LOG, FILE_TYPE_ZIP)));
+    }
+
 }
 
 
---
+++ SsdTestShell/src/main/java/constants/Logging.java
@@ -0,0 +1,14 @@
+package constants;
+
+public class Logging {
+    public static final String LATEST_LOGFILE_NAME = "latest.log";
+    public static final String LOGGING_DATE_FORMAT = "['yy/MM/dd HH:mm']'";
+    public static final int BYTES = 1024;
+    public static final int MAX_FILE_SIZE = 10;
+    public static final String FILE_NAME_DATE_FORMATTER = "yyyyMMdd_HH'h'_mm'm'_ss";
+    public static final String SEPERATED_LOGFILE_PREFIX = "until_";
+    public static final String FILE_TYPE_LOG = ".log";
+    public static final int MAX_LOGFILE_NUMBER = 2;
+    public static final String FILE_TYPE_ZIP = ".zip";
+    public static final String FILE_PATH = "./";
+}
